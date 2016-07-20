34d6831b5c08df5c0ce8099b3078fcdb93590244
jbrownssf <jpbrown@softstackfactory.org>
2016-07-20 07:00:19 +0000

    Adds hooks for the Schedules model to restrict using the calls to members and allowing admins/owners access.

--- common/models/schedule.js
+++ common/models/schedule.js
@@ -1,3 +1,58 @@
 module.exports = function(Schedule) {
-
+    //User can only get it's own inputs
+    Schedule.beforeRemote('find', function(context, instance, next) {
+        var Members = Schedule.app.models.Members;
+        checkMemsUsers(context.req.accessToken.userId);
+        function checkMemsUsers(userId) {
+            Members.find({"where":{"memberId": userId, "orgId": context.args.filter.where.groupId}}, function(memErr, memRes) {
+                if(memErr)
+                    return next(memErr);
+                if(memRes.length === 0 || memRes[0].__data.status === 'pending' || memRes[0].__data.status === 'declined') {
+                    var err = new Error("Unauthorized to perform this action");
+                    err.status = 401;
+                    next(err);
+                } else {
+                    next();
+                }
+            });
+        }
+    });
+    
+    //User can only get it's own inputs
+    Schedule.beforeRemote('findOne', function(context, instance, next) {
+        var Members = Schedule.app.models.Members;
+        checkMemsUsers(context.req.accessToken.userId);
+        function checkMemsUsers(userId) {
+            Members.find({"where":{"memberId": userId, "orgId": context.args.filter.where.groupId}}, function(memErr, memRes) {
+                if(memErr)
+                    return next(memErr);
+                if(memRes.length === 0 || memRes[0].__data.status === 'pending' || memRes[0].__data.status === 'declined') {
+                    var err = new Error("Unauthorized to perform this action");
+                    err.status = 401;
+                    next(err);
+                } else {
+                    next();
+                }
+            });
+        }
+    });
+    
+    //should catch "put" methods
+    Schedule.beforeRemote('upsert', function(ctx, something, next) {
+        var Members = Schedule.app.models.Members;
+        checkMemsUsers(ctx.req.accessToken.userId);
+        function checkMemsUsers(userId) {
+            Members.find({"where":{"memberId": userId, "orgId": ctx.args.data.groupId}}, function(memErr, memRes) {
+                if(memErr)
+                    return next(memErr);
+                if(memRes.length === 0 || (memRes[0].__data.status !== 'admin' && memRes[0].__data.status !== 'owner')) {
+                    var err = new Error("Unauthorized to perform this action");
+                    err.status = 401;
+                    next(err);
+                } else {
+                    next();
+                }
+            });
+        }
+    });
 };
