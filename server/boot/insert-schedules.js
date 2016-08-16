56db74e20beb2e22d48f9c002c2c33d05e26b2e6
jbrownssf <jpbrown@softstackfactory.org>
2016-08-16 00:59:41 +0000

    Comments out the showcase data.

--- server/boot/insert-schedules.js
+++ server/boot/insert-schedules.js
@@ -1,9 +1,9 @@
 module.exports = function(app) {
-	var showcaseData = ['History', 'Members', 'Organizations', 'Schedule', 'SSFUsers'];
-	for(var i in showcaseData) {
-		app.models[showcaseData[i]].destroyAll();
-		app.models[showcaseData[i]].upsert(require('../showcaseData/' + showcaseData[i] + '.json'), function(err, record) {
-			if (err) return console.log(err);
-		});
-	}
+	// var showcaseData = ['History', 'Members', 'Organizations', 'Schedule', 'SSFUsers'];
+	// for(var i in showcaseData) {
+	// 	app.models[showcaseData[i]].destroyAll();
+	// 	app.models[showcaseData[i]].upsert(require('../showcaseData/' + showcaseData[i] + '.json'), function(err, record) {
+	// 		if (err) return console.log(err);
+	// 	});
+	// }
 };
